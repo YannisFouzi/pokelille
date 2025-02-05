4de82e32f60d284bd6f040c276df85009f9694b8
YannisFouzi <yfouzi.dev@gmail.com>
2025-02-05 12:06:11 +0100

    Adjust app layout and remove default color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       {/* Gradient du haut (1/3) */}
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-600 to-green-500 gradient-section" />
 
